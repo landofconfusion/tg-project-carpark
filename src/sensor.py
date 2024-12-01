1959cf4cb596fc0ce7b05d1483e7d646d9308e9c
20135237 <20135237@tafe.wa.edu.au>
2024-12-01 11:21:40 +0800

    fix: fix incorrect way to create sublasses using def rather than class in sensor

--- src/sensor.py
+++ src/sensor.py
@@ -1,17 +1,26 @@
-class Sensor:
-    def __init__(self, id, is_active = False, car_park):
+import random
+from abc import ABC, abstractmethod
+
+class Sensor(ABC):
+    def __init__(self, id, car_park, is_active = False):
         self.id = id
         self.is_active = is_active
         self.car_park = car_park
 
-
     def __str__(self):
         return f'Sensor {self.id} status is {self.is_active}'
 
+    @abstractmethod
+    def update_car_park(self):
+        ...
 
-    def EntrySensor (Sensor):
+    def _scan_plate(self):
+        return 'FAKE' + format(random.randint(0,999), "03d")
+
+
+class EntrySensor(Sensor):
         ...
 
 
-    def ExitSensor (Sensor):
+class ExitSensor(Sensor):
         ...
