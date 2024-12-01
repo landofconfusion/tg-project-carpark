24748d65d0c13ea7ee4407cad693777b8ce5576c
20135237 <20135237@tafe.wa.edu.au>
2024-12-01 11:27:59 +0800

    feat: created a register for the carpark which checks components are either sensor or display before adding to register

--- src/car_park.py
+++ src/car_park.py
@@ -1,3 +1,7 @@
+from display import Display
+from sensor import Sensor
+
+
 class CarPark:
     def __init__(self, capacity, location = "Unknown", plates = None, sensors = None, displays = None):
         self.location = location
@@ -8,3 +12,11 @@ class CarPark:
 
     def __str__(self):
         return f'Car park at {self.location} has {self.capacity} bays'
+
+    def register(self, component):
+        if not isinstance(component, (Sensor, Display)):
+            raise TypeError("Object must be a Sensor or a Display")
+        if isinstance(component, Sensor):
+            self.sensors.append(component)
+        elif isinstance(component, Display):
+            self.displays.append(component)
