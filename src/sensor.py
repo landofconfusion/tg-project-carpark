98f70090836144c263addd7c674c4c039559843a
20135237 <20135237@tafe.wa.edu.au>
2024-12-01 11:01:47 +0800

    feat: added constructor and attributes to sensor and display classes, added subclasses to distinguish sensor type

--- src/sensor.py
+++ src/sensor.py
@@ -1,2 +1,17 @@
 class Sensor:
-    pass
+    def __init__(self, id, is_active = False, car_park):
+        self.id = id
+        self.is_active = is_active
+        self.car_park = car_park
+
+
+    def __str__(self):
+        return f'Sensor {self.id} status is {self.is_active}'
+
+
+    def EntrySensor (Sensor):
+        ...
+
+
+    def ExitSensor (Sensor):
+        ...
